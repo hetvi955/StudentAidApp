4057b6f0cf754557ec7affb67f203da4f55bc84c
gokul-00 <106119141@nitt.edu>
2020-12-11 12:49:36 +0530

    feeds - community part

--- Screens/FeedsScreen.js
+++ Screens/FeedsScreen.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+import Card from "../components/Card";
+import colors from "../config/colors";
+import Screen from "../components/Screen";
+
+const feedPosts = [
+    {
+        id: 1,
+        title: "College",
+        description: `An educational institution or establishment, in particular one providing 
+        higher education or specialized professional or vocational training`,
+        tags: [ 'college', 'students', 'placement' ],
+        image: require("../assets/college.jpg")
+    },
+    {
+        id: 2,
+        title: "Graduation",
+        description: `Graduation is the award of a diploma or academic degree, or the ceremony 
+        that is sometimes associated with it, in which students become graduates.`,
+        tags: [ 'college', 'students', 'graduation', '2020' ],
+        image: require("../assets/graduation.jpg")
+    }
+]
 
 function FeedsScreen(props) {
     return (
-        <View style={styles.container}>
-            <Text>Feeds</Text>
-        </View>
+        <Screen style={styles.screen}>
+            <FlatList
+            data={feedPosts}
+            keyExtractor={(post) => post.id.toString()}
+            renderItem={({ item }) => (
+                <Card
+                    title={item.title}
+                    tags={item.tags}
+                    description={item.description}
+                    image={item.image}
+                    onPress={() => console.log(item)}
+                />
+            )}
+            />
+        </Screen>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-    }
+    screen: {
+        padding: 20,
+        backgroundColor: colors.light,
+    },
 })
 
 export default FeedsScreen;
+
