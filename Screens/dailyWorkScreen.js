759045d2fa5897bd9b1f3e1e37edb8918cbadb39
Sai <saisandhya187@gmail.com>
2020-12-12 22:23:13 +0530

    date for duedates

--- Screens/dailyWorkScreen.js
+++ Screens/dailyWorkScreen.js
@@ -8,9 +8,18 @@ import { TextInput } from "react-native-gesture-handler";
 function DailyWork() {
     const [value, onChangeText] = React.useState('Type your work here');
     const [titleText, onChangeTitle] = React.useState("12-12-2020");
+    const [currentDate, setCurrentDate] = React.useState('');
+    React.useEffect(() => {
+        var date = new Date().getDate(); //Current Date
+        var month = new Date().getMonth() + 1; //Current Month
+        var year = new Date().getFullYear(); //Current Year
+        setCurrentDate(date + '/' + month + '/' + year);
+    }, []);
     return (
         <View style={styles.screen}>
-            <Text>Date: 12.12.2020</Text>
+            <Text>
+                {currentDate}
+            </Text>
             <TextInput
                 style={{ height: 60, borderColor: 'gray', borderWidth: 2 }}
                 onChangeText={text => onChangeText(text)}
