5164eef8467b80835fa9efc2cc5dc462f540686b
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2020-12-15 11:41:27 +0530

    update functional component

--- Screens/UpdateApi.js
+++ Screens/UpdateApi.js
@@ -1,73 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {View, Text, ActivityIndicator, StatusBar, StyleSheet,  Linking, ScrollView, SafeAreaView} from 'react-native';
-import { create } from 'apisauce';
-import { Component } from 'react';
-import { FlatList } from 'react-native-gesture-handler';
+
+function Update(props) {
+  const [data, setData]= useState([]);
+  const [loading, setLoading]= useState(true);
 
 
-/* // define the api
-const api = create({
-  baseURL: 'http://api.adzuna.com/v1/api',
-  headers: { timeout: 20000 },
-}) */
+function getData() {
+    return (
+      fetch('https://api.adzuna.com/v1/api/jobs/in/search/2?app_id=6fb4d871&app_key=51579f6594c9d91943719f071dfcff72&results_per_page=15')
+      .then((response)=> response.json())
+    )
+  }
+  
+  useEffect(()=>{
+    getData()
+    .then((responseJson)=>{
+     setData(responseJson.results),
+     setLoading(false)
+    })
+    .catch(err=> console.log(err))
+  },[]);
 
+    if(loading) {
+      return (
+        <ScrollView style={{flex: 1, padding:10}}>
+          <ActivityIndicator/>
+        </ScrollView>
+      ) 
+      
+     }else{
+       let updates= data.map((val, key)=>{
+         return <View key={key} >
+           
+           <Text style={styles.head}>{val.company.display_name}</Text>
+           <Text style={styles.type}>{val.contract_type}</Text>
+           <Text style={styles.location}>{val.location.area}</Text>
+           <Text style={styles.desc}>{val.description}</Text>
+           <Text style={styles.link}
+                onPress={() => Linking.openURL(val.redirect_url)}>
+            Read more..
+          </Text>
+         </View>
+       })
+      return (
+        <ScrollView style={styles.container}>
+          <Text style={styles.top}>Here's your weekly Job update! </Text>
+           {updates} 
+        </ScrollView>
+       )
+     }
+   
+  }
 
-export default class Updates extends Component{
-        constructor(props){
-          super(props);
-          this.state= {
-            data: null,
-            isLoading: true,
-
-          }
-        }
-        componentDidMount(){
-          return (
-            fetch('https://api.adzuna.com/v1/api/jobs/in/search/2?app_id=6fb4d871&app_key=51579f6594c9d91943719f071dfcff72&results_per_page=15')
-            .then((response)=> response.json())
-            .then((responseJson)=>{
-              this.setState({
-                isLoading: false,
-                data: responseJson.results
-              })
-            })
-            .catch(err=> console.log(err))
-            )
-        }
-
-        render(){
-
-          if(this.state.isLoading) {
-            return (
-              <ScrollView style={{flex: 1, padding:10}}>
-                <ActivityIndicator/>
-              </ScrollView>
-            ) 
-            
-           }else{
-             let updates= this.state.data.map((val, key)=>{
-               return <View key={key} >
-                 
-                 <Text style={styles.head}>{val.company.display_name}</Text>
-                 <Text style={styles.type}>{val.contract_type}</Text>
-                 <Text style={styles.location}>{val.location.area}</Text>
-                 <Text style={styles.desc}>{val.description}</Text>
-                 <Text style={styles.link}
-                      onPress={() => Linking.openURL(val.redirect_url)}>
-                  Read more..
-                </Text>
-               </View>
-             })
-            return (
-              <ScrollView style={styles.container}>
-                <Text style={styles.top}>Here's your weekly Job update! </Text>
-                 {updates} 
-              </ScrollView>
-             )
-           }
-        }
-        
-    }
+export default Update;
 
     const styles = StyleSheet.create({
      
