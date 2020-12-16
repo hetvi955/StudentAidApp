2bef8f6ac5c0b68974e2289fc3813170c4ffbcdf
Sai <saisandhya187@gmail.com>
2020-12-16 09:24:13 +0530

    basic structure of daily work feature

--- Screens/addNoteScreen.js
+++ Screens/addNoteScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
 import { ScrollView } from "react-native-gesture-handler";
@@ -49,6 +50,38 @@ export default function NewNote(props) {
             </Screen>
         </ScrollView>
 
+=======
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Button } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function NewNote() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const changeTitle = (val) => {
+        setTitle(val);
+    };
+    const changeBody = (val) => {
+        setBody(val);
+    };
+    return (
+        <View>
+            <View style={styles.body}>
+                <TextInput
+                    placeholder="Title of the note"
+                    style={styles.titleNote}
+                    onChangeText={changeTitle}
+                />
+                <TextInput
+                    multiline
+                    placeholder="Your note goes here"
+                    style={styles.bodyNote}
+                    onChangeText={changeBody}
+                />
+            </View>
+            <Button title="Save" onPress={() => console.log(title, body)} />
+        </View>
+>>>>>>> 213ac65... basic structure of daily work feature
     );
 }
 
