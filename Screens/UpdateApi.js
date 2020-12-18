6e78dbfbdeabec61943fc9a1ae273a44b5a7f675
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2020-12-18 16:31:35 +0530

    category search

--- Screens/UpdateApi.js
+++ Screens/UpdateApi.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import {View, Text, ActivityIndicator, StatusBar, StyleSheet,  Linking, ScrollView, SafeAreaView} from 'react-native';
+import {View, Text, ActivityIndicator, StatusBar, StyleSheet,  Linking, ScrollView, SafeAreaView, TextInput} from 'react-native';
 
-function Update(props) {
+function Update() {
   const [data, setData]= useState([]);
   const [loading, setLoading]= useState(true);
+  const [query, setQuery]= useState("");
 
+  const baseurl="https://api.adzuna.com/v1/api/jobs/in/search/2?app_id=05f5b785&app_key=d7a69091c7366d575983bb8b0afad7b0&results_per_page=15&what_and="+{query};
 
 function getData() {
     return (
-      fetch('https://api.adzuna.com/v1/api/jobs/in/search/2?app_id=6fb4d871&app_key=51579f6594c9d91943719f071dfcff72&results_per_page=15')
+      fetch(baseurl)
       .then((response)=> response.json())
     )
   }
-  
+
   useEffect(()=>{
     getData()
     .then((responseJson)=>{
      setData(responseJson.results),
      setLoading(false)
-    })
+    }) 
     .catch(err=> console.log(err))
   },[]);
 
@@ -43,14 +45,18 @@ function getData() {
           </Text>
          </View>
        })
+
       return (
         <ScrollView style={styles.container}>
           <Text style={styles.top}>Here's your weekly Job update! </Text>
+          <Text style={styles.search}>Couldn't find what you are looking for? Search here. </Text>
+          <TextInput value={query} placeholder="Search category" onChangeText={(query)=>{setQuery(query), console.log(query), console.log(baseurl)}}  
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 , margin:10 , borderRadius: 5 , padding: 10}} />
            {updates} 
         </ScrollView>
+        
        )
      }
-   
   }
 
 export default Update;
@@ -67,7 +73,7 @@ export default Update;
         marginTop:15,
         fontWeight: "100",
         fontSize: 23,
-        color:"blue",
+        color:"olive",
         fontFamily:"Roboto"
       },
       head: {
@@ -91,6 +97,13 @@ export default Update;
         padding:5,
         fontSize: 13,
         fontFamily:'sans-serif-condensed'
+      },
+      search: {
+        marginLeft:10,
+        paddingTop:8,
+        fontSize: 11,
+        fontFamily:'sans-serif-light',
+        color:"grey"
       },
       link: {
         marginLeft:10,
